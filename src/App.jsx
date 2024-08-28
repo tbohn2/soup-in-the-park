035501234c40f1b93812466828e3db89ea0ddd42
tbohn2 <tanner.bohn@yahoo.com>
2024-08-28 15:32:29 -0700

    Sign Up page at path '/'

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import SignUp from './pages/SignUp';
 import './App.css'
 
 function App() {
@@ -20,7 +21,9 @@ function App() {
 
   return (
     <Router>
-
+      <Routes>
+        <Route path="/" element={<SignUp mobile={mobile} />} />
+      </Routes>
     </Router>
   )
 }
