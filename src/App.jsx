90602d3c4270d9955f14860378b563dd74fdf372
tbohn2 <tanner.bohn@yahoo.com>
2024-09-12 18:41:19 -0700

    Changed position of nav and added class when active; created background image; added spinning animation to loading image

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,20 @@ import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import SignUp from './pages/SignUp';
 import Gallery from './pages/Gallery';
+import logo from './assets/logo.png';
 import './App.css'
 
 function App() {
   const [mobile, setMobile] = useState(false);
+  const [signUp, setSignUp] = useState(true);
+
+  const checkNav = () => {
+    if (window.location.pathname === '/gallery') {
+      setSignUp(false);
+    } else {
+      setSignUp(true);
+    }
+  }
 
   useEffect(() => {
     if (window.innerWidth <= 768) {
@@ -21,18 +31,21 @@ function App() {
   }, []);
 
   return (
+
     <Router basename='/soup-in-the-park'>
-      <header className='bg-light'>
-        <h1 className='kaushan text-center col-12'>Soup In The Park</h1>
-        <nav className='chewy fs-3 col-12 d-flex justify-content-evenly'>
-          <Link to='/' className='navbar-brand'>Sign-up</Link>
-          <Link to='/gallery' className='navbar-brand'>Gallery</Link>
-        </nav>
-      </header>
-      <Routes>
-        <Route path="/" element={<SignUp mobile={mobile} />} />
-        <Route path="/gallery" element={<Gallery mobile={mobile} />} />
-      </Routes>
+      <div className='bg-yellow text-blue'>
+        <header className='d-flex flex-column align-items-center'>
+          <img src={logo} alt="" />
+          <nav className='chewy fs-3 d-flex justify-content-evenly'>
+            <Link to='/' className={`nav-btn ${signUp ? 'text-decoration-underline' : ''}`} onClick={() => setSignUp(true)}>Sign-up</Link>
+            <Link to='/gallery' className={`nav-btn ${!signUp ? 'text-decoration-underline' : ''}`} onClick={() => setSignUp(false)}>Gallery</Link>
+          </nav>
+        </header>
+        <Routes>
+          <Route path="/" element={<SignUp mobile={mobile} />} />
+          <Route path="/gallery" element={<Gallery mobile={mobile} />} />
+        </Routes>
+      </div>
     </Router>
   )
 }
