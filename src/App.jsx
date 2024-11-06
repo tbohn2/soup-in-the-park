9ce40a979de435ddc38e992e21bfa69c203313a5
tbohn2 <tanner.bohn@yahoo.com>
2024-11-06 15:42:49 -0700

    Created copies of gallery and sign up pages for christmas event; added new logo; made root colors with generic names

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,11 @@ import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import SignUp from './pages/SignUp';
 import Gallery from './pages/Gallery';
-import logo from './assets/logo.png';
+import ChristmasSignUp from './pages/ChrismasSignUp';
+import ChristmasGallery from './pages/ChristmasGallery';
+// import logo from './assets/logo.png';
+import logo from './assets/trumpSanty.png';
+
 import './App.css'
 
 function App() {
@@ -58,8 +62,10 @@ function App() {
           }
         </header>
         <Routes>
-          <Route path="/" element={<SignUp mobile={mobile} />} />
-          <Route path="/gallery" element={<Gallery mobile={mobile} />} />
+          <Route path="/" element={<ChristmasSignUp mobile={mobile} />} />
+          <Route path="/gallery" element={<ChristmasGallery mobile={mobile} />} />
+          {/* <Route path="/" element={<SignUp mobile={mobile} />} />
+          <Route path="/gallery" element={<Gallery mobile={mobile} />} /> */}
         </Routes>
       </div>
     </Router >
---
+++ src/pages/ChristmasGallery.jsx
@@ -0,0 +1,96 @@
+import React, { useState, useEffect } from 'react';
+import CompressedImg from '../components/CompressedImg.jsx';
+import '../styles/gallery.css'
+
+const pics2005 = import.meta.glob('../assets/2005/*.jpg', { eager: true });
+
+const urlArray = [];
+
+function addToUrlArray(picObj) {
+    urlArray.push(...Object.keys(picObj).map((key) => picObj[key].default));
+}
+
+addToUrlArray(pics2005);
+
+const ChristmasGallery = () => {
+
+    const [loading, setLoading] = useState(false);
+    const [picUrls, setPicUrls] = useState([]);
+    const [focusedURL, setFocusedURL] = useState({
+        url: null,
+        index: null
+    });
+
+    useEffect(() => {
+        setPicUrls(urlArray);
+    }, []);
+
+    const handleNext = () => {
+        setLoading(true);
+        setFocusedURL(prev => {
+            const nextIndex = (prev.index + 1) % picUrls.length;
+            return { url: picUrls[nextIndex], index: nextIndex };
+        });
+    };
+
+    const handlePrev = () => {
+        setLoading(true);
+        setFocusedURL(prev => {
+            const prevIndex = (prev.index - 1 + picUrls.length) % picUrls.length;
+            return { url: picUrls[prevIndex], index: prevIndex };
+        });
+    };
+
+    const handleKeyDown = (event) => {
+        if (!focusedURL.url) return;
+
+        switch (event.key) {
+            case 'ArrowLeft':
+                handlePrev();
+                break;
+            case 'ArrowRight':
+                handleNext();
+                break;
+            case 'Escape':
+                setFocusedURL({ url: null, index: null });
+                break;
+            default:
+                break;
+        }
+    };
+
+    const handleImageLoad = () => {
+        setLoading(false);
+    };
+
+    useEffect(() => {
+        if (focusedURL.url) {
+            window.addEventListener('keydown', handleKeyDown);
+
+            return () => {
+                window.removeEventListener('keydown', handleKeyDown);
+            };
+        }
+    }, [focusedURL]);
+
+    return (
+        <div id='gallery' className='main-content fade-in d-flex flex-column align-items-center'>
+            <h1 className='kaushan'>Over the Years</h1>
+            <div className='d-flex flex-wrap justify-content-evenly'>
+                {picUrls.map((url, index) => (
+                    <CompressedImg url={url} index={index} setFocusedURL={setFocusedURL} />
+                ))}
+            </div>
+            {focusedURL.url &&
+                <div id='focused-img' className={focusedURL ? 'show' : ''}>
+                    {loading && <div className="spinner-border" role="status"></div>}
+                    <button id='close-btn' onClick={() => setFocusedURL({ url: null, index: null })}>&times;</button>
+                    <button id='prev-btn' className='gal-nav-btn' onClick={handlePrev}>&lt;</button>
+                    <button id='next-btn' className='gal-nav-btn' onClick={handleNext}>&gt;</button>
+                    <img className='fade-in' src={focusedURL.url} alt='' onLoad={handleImageLoad} />
+                </div>}
+        </div>
+    );
+};
+
+export default ChristmasGallery;
